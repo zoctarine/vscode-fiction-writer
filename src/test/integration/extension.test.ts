2bafff293d4641660474b5cc74b805df998d32d3
Andrei <andreinzorila@gmail.com>
2021-03-02 19:54:49 +0200

    Use mocha for integration tests and jest for unit tests

--- src/test/integration/extension.test.ts
+++ src/test/integration/extension.test.ts
@@ -1,10 +1,8 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
-import { resolveCliPathFromVSCodeExecutablePath } from 'vscode-test';
-vscode.window.showInformationMessage("TEST");
 
-describe('Extension Test Suite', () => {
-	it('Sample test', () => {
+suite('Extension Test Suite', () => {
+	test('Sample test', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
 	});
